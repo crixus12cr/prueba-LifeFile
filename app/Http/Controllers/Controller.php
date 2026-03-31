92614ca1e26fdc9bc413f211364712ec120f48b8
crixus12cr <cristian2020til@gmail.com>
2026-03-31 12:10:29 -0500

    feat: add Customer model with corresponding migration

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 abstract class Controller
 {
     //
+    function FunctionName() : Returntype {
+        return $this->response()->json([
+            'message' => 'Hello, World!',
+        ],JsonResponse::HTTP_OK);
+    }
 }
---
+++ app/Models/Customer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    protected $fillable = [
+        'name', 
+        'email', 
+        'phone'
+    ];
+
+    public function orders() {
+        return $this->hasMany(Order::class);
+    }
+
+    public function alerts() {
+        return $this->hasMany(Alert::class);
+    }
+}
