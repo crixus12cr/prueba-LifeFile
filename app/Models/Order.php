81174548c6482c29bbf2de866ba1e556dab9fcc2
crixus12cr <cristian2020til@gmail.com>
2026-03-31 12:20:13 -0500

    feat: add Order model with corresponding migration

---
+++ app/Models/Order.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'customer_id', 
+        'purchase_date'
+        ];
+
+    protected $casts = [
+        'purchase_date' => 'date',
+    ];
+
+    public function customer() {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function orderItems() {
+        return $this->hasMany(OrderItem::class);
+    }
+}
