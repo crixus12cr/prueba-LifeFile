58b7b6f374f1a2c119839ec65e0dd3c8ef9148e3
crixus12cr <cristian2020til@gmail.com>
2026-03-31 16:39:09 -0500

    feat: add OrderItem model and migration

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    protected $fillable = [
+        'order_id', 
+        'medication_id'
+    ];
+
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function medication() {
+        return $this->belongsTo(Medication::class);
+    }
+}
