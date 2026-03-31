b91179e098772a681566a8d59b49bc4c1f89834d
crixus12cr <cristian2020til@gmail.com>
2026-03-31 10:50:57 -0500

    feat: initialize Vue app with Vue Router

---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container mx-auto p-8 text-center">
+        <h1 class="text-4xl font-bold text-green-600 mb-4">
+            ¡Configuacion VUE!
+        </h1>
+        <p class="text-xl text-gray-600">
+            Laravel y Vue monolito funcionando correctamente
+        </p>
+        <div class="mt-8">
+            <p class="text-green-500"> API con Sanctum configurada</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Welcome'
+}
+</script>
